fee983a14ea98d471d15cff8936ffad04d0f989d
ChangJoo Park <changjoo.park@mealant.com>
2019-05-10 12:23:28 +0900

    Add soundfont-player to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Soundfont from 'soundfont-player'
 import omit from "lodash/omit";
 import Staff from "./Staff";
 import Tabs from "./Tabs";
@@ -13,9 +14,21 @@ export default function App() {
       : setNotes({ ...notes, [column]: note });
   };
 
+  const onPlayClick = () => {
+    const AudioContext = window['AudioContext']
+    Soundfont.instrument(new AudioContext(), 'acoustic_guitar_nylon')
+      .then(function (acoustic_guitar_nylon) {
+        Object.keys(notes).forEach(key => {
+          acoustic_guitar_nylon.play(notes[key])
+        })
+      })
+  }
   return (
     <div className="App">
       <h1>Notes to Ukulele Tabs</h1>
+
+      <button onClick={onPlayClick}>Play!</button>
+
       <p className="App__hint">
         Hint: Click on staff to place a note. Click on a tab to toggle the tab.
       </p>
